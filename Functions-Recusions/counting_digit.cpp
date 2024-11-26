7acb2a5d05502723bc458b115c8f6648b2591f21
Raja-Sherhyar-Ameer <shehryarameer564@gmail.com>
2024-11-26 09:11:04 +0500

    new commit

--- Functions-Recusions/counting_digit.cpp
+++ Functions-Recusions/counting_digit.cpp
@@ -1,11 +1,22 @@
 #include<iostream>
 using namespace std;
+int digit_counter(int val,int &count){
+    if(val==0){
+        return count; 
+    }
+    else{
+        count+=1;
+        return digit_counter(val/10,count);
+    }
+
+}
 int main(){
 
-    
+int num,counter=0;
+cout<<"Enter the number: ";
+cin>>num;
 
-
-
+cout<<digit_counter(num,counter);
 
     return 0;
 }
