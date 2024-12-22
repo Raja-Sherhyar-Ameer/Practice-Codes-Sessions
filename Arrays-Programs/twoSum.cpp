dd18793a1e0dd2940028b83465314ce65a3d9f5a
Raja-Shehryar-Ameer <shehryarameer564@gmail.com>
2024-12-22 20:01:42 +0500

    debugging the two sum program

--- Arrays-Programs/twoSum.cpp
+++ Arrays-Programs/twoSum.cpp
@@ -14,6 +14,31 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
+    int* arr_ptr=arr;
+
+    int index=twoSum(arr_ptr,n,6);
+    if(index!=-1){
+        cout<<"Targeted sum is found and formed by adding index at:: ";
+        for(int i=0;i<=index;i++){
+            cout<<i<<" ";
+        }
+    }
+    else{
+        cout<<"Targeted sum does not found";
+    }
 
     return 0;
+}
+int twoSum(int* ptr, int size, int target){
+    int sum=0;
+
+    for(int i=0;i<size;i++){
+        sum+=(*(ptr+i));
+
+        if(target==sum){
+            return i;
+        }
+    }
+
+    return -1;
 }
