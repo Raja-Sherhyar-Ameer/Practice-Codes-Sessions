b80de8fe7a92dcad953e9802f429cdaa06332f97
Raja-Shehryar-Ameer <shehryarameer564@gmail.com>
2024-12-22 12:38:29 +0500

    made max-sub_array sum

--- Arrays-Programs/max_sub-array_sum.cpp
+++ Arrays-Programs/max_sub-array_sum.cpp
@@ -13,4 +13,28 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
+
+    cout<<"The maximum sub array sum through 1st approach is " <<max_sum_1(arr,n)<<endl;
+
+}
+
+int max_sum_1(int arr[], int size){
+    int sum=0,max_sum=INT_MIN;
+
+    for(int sub=1;sub<size;sub++){  // sub-array counter
+
+        // possible sub-array combination
+        for(int i=0;i<=size-sub;i++){
+            for(int j=i;j<i+sub;j++){
+                sum+=arr[j];
+            }
+
+            if(sum>max_sum){
+                max_sum=sum;
+            }
+            sum=0;
+        }
+    }
+
+    return max_sum;
 }
