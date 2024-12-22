71a2756f537d9948e2f3c861f82918c4c844d48a
Raja-Shehryar-Ameer <shehryarameer564@gmail.com>
2024-12-22 20:20:01 +0500

    solving max-substring palindrome problem

---
+++ Strings-Play/max_palindrome_sub_string.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string word;
+    cout<<"Enter the word: ";
+    getline(cin,word);
+    
+    bool isPalindrome=true;
+    string temp="", palindrome="";
+    int c_length=0,p_length=0;
+    
+    // subset counter
+    for(int k=1;k<=word.length();k++){
+        for(int i=0;i<=word.length()-k;i++){
+            
+            // forming sub-string
+            for(int j=i;j<i+k;j++){
+                temp+=word[j];
+            }
+            
+            c_length=temp.length()-1;
+
+            // checking palindrome
+            for(int x=0;x<(temp.length())/2;x++){
+                if(temp[x]!=temp[c_length]){
+                    isPalindrome=false;
+                    break;
+                }
+                else{
+                    isPalindrome=true;
+                    c_length--;
+                }
+            }
+
+            // cehcking largest subset palindrome
+            if(isPalindrome &&  temp.length()>p_length){
+                p_length=temp.length();
+                palindrome=temp;
+            }
+            
+            temp="";
+        }
+    }
+    
+    // printing the largest palindrome
+    cout<<"Largest Sub-strings:: "<<palindrome<<endl;
+    
+    return 0;
+}
