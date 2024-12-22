420b2b48d2565bef1dce6a997acd061724f07f57
Raja-Shehryar-Ameer <shehryarameer564@gmail.com>
2024-12-22 12:21:47 +0500

    inputting the array and its relevant variable

--- Arrays-Programs/max_sub-array_sum.cpp
+++ Arrays-Programs/max_sub-array_sum.cpp
@@ -4,5 +4,13 @@ using namespace std;
 int max_sum_1(int arr[], int size);
 
 int main(){
+    int arr[100];
+    int n;
+    cout<<"Enter the size of the array: ";
+    cin>>n;
 
+    // inputting the array
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
 }
