d225f4d0027c991ee4364e99f3f08cdb023cd331
Raja-Shehryar-Ameer <shehryarameer564@gmail.com>
2024-12-22 15:35:20 +0500

    write the function signature and manage inputs for two sum problem

---
+++ Arrays-Programs/twoSum.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int twoSum(int* ptr, int size, int target);
+
+int main(){
+    
+    int arr[100];
+    int n;
+    cout<<"Enter the size of matrix: ";
+    cin>>n;
+    
+    // inputting the array
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    return 0;
+}
