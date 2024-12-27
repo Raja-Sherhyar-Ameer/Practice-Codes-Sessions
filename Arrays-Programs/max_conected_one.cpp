8c880a2a7de8c15e57563084cdfcf529b4262812
Raja-Shehryar-Ameer <shehryarameer564@gmail.com>
2024-12-27 13:06:07 +0500

    Debugging the connected one error

--- Arrays-Programs/max_conected_one.cpp
+++ Arrays-Programs/max_conected_one.cpp
@@ -13,7 +13,34 @@ int main(){
         }
     }
 
-    
+    int temp=INT_MIN;
+    for(int i=0;i<size;i++){
+        for(int j=0;j<size;j++){
+            int counter=0,sum=0;
+            while(counter<size*size){
+                int x=i,y=j;
+
+                if(x+1<size && matrix[x][y]==1){       // move down
+                    sum+=matrix[x++][y];
+                }
+                else if(y-1>=0 && matrix[x][y]==1){       // move left
+                    sum+=matrix[x][y--];
+                }
+                else if(y+1<size && matrix[x][y]==1){       // move right
+                    sum+=matrix[x][y++];
+                }
+                else if(x-1>=0 && matrix[x][y]==1){       // move left
+                    sum+=matrix[x--][y];
+                }
+                counter++;
+            }
+            if(sum>temp){
+                temp=sum;
+            }
+        }
+    }
+
+    cout<<temp<<endl;
 
 
     return 0;
