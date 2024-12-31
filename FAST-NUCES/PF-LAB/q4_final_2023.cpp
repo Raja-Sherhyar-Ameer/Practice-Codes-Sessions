ecbdfba83158860e033ef036b67ac37fa1667a6e
Raja-Shehryar-Ameer <shehryarameer564@gmail.com>
2024-12-31 22:45:12 +0500

    added comments in the code for better understanding and comprehension

--- FAST-NUCES/PF-LAB/q4_final_2023.cpp
+++ FAST-NUCES/PF-LAB/q4_final_2023.cpp
@@ -5,8 +5,8 @@ bool isSubstringfound(string sentence,string substring);
 void replaceSubstring(string sentence,string substring, string replace);
 int main(){
 
-    
-    string sentence = "I am Pakistani so I support Pakistani Cricket team in Pak-India match";
+    // inputing all the data from user
+    string sentence = "I am king of king's ship";
     string substring,replace_string;
     cout<<"Enter the substring: ";
     cin>>substring;
@@ -18,6 +18,7 @@ int main(){
     return 0;
 }
 
+// used to check whether the substring exist
 bool isSubstringfound(string sentence,string substring){
 
     for(int k=0;k<sentence.length();k++){
@@ -33,16 +34,18 @@ bool isSubstringfound(string sentence,string substring){
     return 0;
 }
 
-
+// used to replace the substring with new string
 void replaceSubstring(string sentence,string substring, string replace){
     string temp="";
 
     if(isSubstringfound(sentence,substring)){ 
-
+        
+        // iterating through the whole sentence
         for(int i=0;i<sentence.length();){
 
             if(sentence[i]==substring[0]){
-
+                
+                // replacement with the required word
                 for(int j=0;j<replace.length();j++){
                     temp+=replace[j];
                 }
