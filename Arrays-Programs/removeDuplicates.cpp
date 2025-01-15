55ba90a5193631abfb9ce72c98125294a526c71a
Raja-Shehryar-Ameer <shehryarameer564@gmail.com>
2025-01-15 21:24:40 +0500

    debugged the sorting algo--bubble sort

--- Arrays-Programs/removeDuplicates.cpp
+++ Arrays-Programs/removeDuplicates.cpp
@@ -15,19 +15,18 @@ int main(){
 
     // sorting the array
     for(int i=0;i<length;i++){
-        for(int j=i+1;j<length-i;j++){
+        for(int j=0;j<length-i-1;j++){
             
-            if(arr[i]>arr[j]){
-                int temp=arr[i];
-                arr[i]=arr[j];
-                arr[j]=temp;
+            // comparing the element and finding max, placing at right position
+            if(arr[j]>arr[j+1]){
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
             }
-            
+
         }
     }
 
-
-    
-    
+   
     return 0;
 }
