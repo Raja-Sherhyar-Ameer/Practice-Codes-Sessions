a8141f135a8b905e832fd243e1a854ba68ca01ea
Raja-Shehryar-Ameer <shehryarameer564@gmail.com>
2025-03-22 06:55:50 +0500

    Add some bounding checks in accordance with the Cramer rule and converted the program to DMA

--- FAST-NUCES/OOPs-LAB/mid_mock_q1.cpp
+++ FAST-NUCES/OOPs-LAB/mid_mock_q1.cpp
@@ -3,7 +3,96 @@ using namespace std;
 
 // Raja Shehryar Ameer    24I - 0544
 
-int determinant_2D_finder(int arr[][3],int blocker){
+int determinant_2D_finder(int**,int);
+void coloumnSwap(int**,int**,int);
+
+int main(){
+
+    // Dyanmically Allocating the memory for 3x3 and 3x1 matrix
+    int** mat = new int*[3];
+    for(int i = 0; i < 3; ++i){
+        mat[i] = new int[3];
+    }
+
+    cout<<"Enter the elements of the 3x3 matrix:"<<endl;
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++) {
+            cin>>mat[i][j];
+        }
+    }
+
+    int** B_mat=new int*[3];
+    for(int i=0;i<3;i++) {
+        B_mat[i]=new int[1];
+    }
+
+    cout << "Enter the elements of the 3x1 matrix:" << endl;
+    for(int i = 0; i < 3; ++i) {
+        cin >> B_mat[i][0];
+    }
+    
+    // Finding of determinant of original matrix
+    int det=0;
+    for(int i=0;i<3;i++){
+
+        if(i&1){
+            det=det-(mat[0][i]* (determinant_2D_finder(mat,i)));
+        }
+        else{
+            det=det+(mat[0][i]*(determinant_2D_finder(mat,i)));
+        }
+    }
+
+    // Bounding Checkque
+    if(det==0){
+        cout<<"No solution exist"<<endl;
+
+        // Avoiding memory leaks
+        for(int i=0;i<3;i++){
+            delete [] B_mat[i];
+            delete[] mat[i];
+        }
+        delete[] B_mat, delete[] mat;
+        B_mat=nullptr,mat=nullptr;
+
+        return 0;
+    }
+    
+
+    // Applying the cramer Rule
+    for(int i=0;i<3;i++){
+        int t_det=0;
+        coloumnSwap(mat,B_mat,i);    // Used to swap respective coloumn
+
+        for(int i=0;i<3;i++){
+
+            if(i&1){
+                t_det=t_det-(mat[0][i]* (determinant_2D_finder(mat,i)));
+            }
+            else{
+                t_det=t_det+(mat[0][i]*(determinant_2D_finder(mat,i)));
+            }
+        }
+        double ans=(float)(t_det/det);
+
+        // Printing the values
+        cout<<"Ans 0"<<i+1<<" "<<ans<<endl;
+        coloumnSwap(mat,B_mat,i);   // Used to recover the matrix
+    }
+
+    // Avoiding memory leaks
+    for(int i=0;i<3;i++){
+        delete [] B_mat[i];
+        delete[] mat[i];
+    }
+    delete[] B_mat, delete[] mat;
+    B_mat=nullptr,mat=nullptr;
+
+    
+    return 0;
+}
+
+int determinant_2D_finder(int** arr,int blocker){
     
     int col_incrementer= blocker&1 ? 2:1,ops=0, det=0;
     
@@ -32,56 +121,7 @@ int determinant_2D_finder(int arr[][3],int blocker){
     
 }
 
-void coloumnSwap(int arr[][3],int swaper[][1],int col);
-
-int main(){
-    int mat[3][3]={
-        {0, -2, 3},
-        {4, -5, 0},
-        {7, 0, -9}
-    };  
-    int B_mat[3][1]={
-        {-1},
-        {2},
-        {3}
-    };
-    
-    int det=0;
-    for(int i=0;i<3;i++){
-
-        if(i&1){
-            det=det-(mat[0][i]* (determinant_2D_finder(mat,i)));
-        }
-        else{
-            det=det+(mat[0][i]*(determinant_2D_finder(mat,i)));
-        }
-    }
-    
-    for(int i=0;i<3;i++){
-        int t_det=0;
-        coloumnSwap(mat,B_mat,i);    // Used to swap respective coloumn
-
-        for(int i=0;i<3;i++){
-
-            if(i&1){
-                t_det=t_det-(mat[0][i]* (determinant_2D_finder(mat,i)));
-            }
-            else{
-                t_det=t_det+(mat[0][i]*(determinant_2D_finder(mat,i)));
-            }
-        }
-        double ans=(float)(t_det/det);
-        cout<<"Ans 0"<<i+1<<" "<<ans<<"  det:"<<t_det<<endl;
-        coloumnSwap(mat,B_mat,i);   // Used to recover the matrix
-    }
-
-
-    cout<<"Determinant: "<<det<<endl;
-    
-    return 0;
-}
-
-void coloumnSwap(int arr[][3],int swaper[][1],int col){
+void coloumnSwap(int** arr,int** swaper,int col){
 
     for(int i=0;i<3;i++){
         int temp=arr[i][col];
